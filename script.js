64234bd8b6c10d556995b33c4e90b23bf9bcffb4
marwankhalaf <114322450+marwankhalaf@users.noreply.github.com>
2023-03-30 22:18:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// start logic 
+
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
